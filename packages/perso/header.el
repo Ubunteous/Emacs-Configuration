98da8feb9204b84ef5d928aebc811194eb6e33f3
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2022-11-26 13:45:37 +0000

    header.el: removed s.el in favour of built-in functions

--- packages/perso/header.el
+++ packages/perso/header.el
@@ -2,23 +2,15 @@
 ;;               HEADER               ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package s)
-
 (defun insert-header ()
   "insert a nice header with the current buffer name at the beginning of said buffer"
   (interactive)
 
   (setq border ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
-
-  (setq name
-	(upcase (nth 0 (s-split "\\." (buffer-name)))))
+  (setq name (upcase (file-name-sans-extension (buffer-name))))
 
   (setq len-name (length name))
-
   (setq nb-spaces (/ (- (length border) len-name 4) 2))
-
-  (message "%s" (int-to-string nb-spaces))
-
   (setq white-insert (make-string nb-spaces ? ))
 
   (goto-char (point-min))
