ab5a7260c2fa740966b4497267db2b16a175da1a
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2022-12-12 18:16:43 +0000

    Moved some useful packages from later/ to packages/*

--- packages/utilities/expand-region.el
+++ packages/utilities/expand-region.el
@@ -2,7 +2,9 @@
 ;;           EXPAND-REGION            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Note: pressing 0 or C-g stops the expansion
+;; Note: expand region has its own repeat map (=)
+
 (use-package expand-region
   :defer t
   :general ("C-=" 'er/expand-region))
-
--- packages/utilities/wgrep.el
+++ packages/utilities/wgrep.el
@@ -3,4 +3,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package wgrep
-  :defer t)
+  :defer t
+  :config
+  (setq wgrep-auto-save-buffer t)
+  ;; change default key binding to switch to wgrep
+  ;; (setq wgrep-enable-key "r")
+  ;; apply all changes even if read-only
+  ;; (setq wgrep-change-readonly-file t)
+  )
