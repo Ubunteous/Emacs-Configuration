a62633c1b182ac2525783052f25339231235df45
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2022-12-13 18:19:23 +0000

    New auto insert code for elisp and org headers

--- packages/built-in/auto-insert.el
+++ packages/built-in/auto-insert.el
@@ -2,6 +2,13 @@
 ;;             AUTO-INSERT            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defun org-header ()
+  "Add a title to an org file.
+   Roam file which start with an ID do not need an extra title"
+  (unless (string-match-p "[0-9]+-.+.org" (buffer-name))
+    (insert (concat "#+title: " (file-name-sans-extension (buffer-name)) "\n\n"))))
+
+
 (use-package autoinsert
   :straight (:type built-in)
   :defer t
@@ -9,20 +16,15 @@
   :config
   ;; (auto-insert-mode 1)
   (setq auto-insert-query nil)
-  (setq auto-insert-alist nil) ;; remove this like to restore defaults
+  (setq auto-insert-alist nil) ;; remove this to restore defaults
 
   ;; add tempel-x later
   ;; (add-to-list 'auto-insert-alist  '(emacs-lisp-mode . [ (lambda () (tempel-insert 'header)) ] )))
-
+  
   ;; org
-  (add-to-list 'auto-insert-alist
-	       '(org-mode .
-			  [(concat
-			    "#+title: "
-			    (file-name-sans-extension (buffer-name))
-			    "\n\n")]))
+  (add-to-list 'auto-insert-alist '(org-mode . [org-header]))
 
   ;; elisp
-  (add-to-list 'auto-insert-alist  '(emacs-lisp-mode . [insert-header] ))
+  (add-to-list 'auto-insert-alist  '(emacs-lisp-mode . [insert-header]))
   :hook
   (find-file . auto-insert))
