2ee459a7b3c784957a8405c42625dd983ad07d1b
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2022-12-14 10:43:39 +0000

    Org modern checkboxes size increased with new icons

--- packages/org/org-modern.el
+++ packages/org/org-modern.el
@@ -18,21 +18,32 @@
    org-modern-list '((43 . "+")  ;; +
 		     (45 . "-")  ;; -
 		     (42 . "•")) ;; *
-   
+
    ;; Org styling, hide markup etc.
    org-hide-emphasis-markers t
    org-pretty-entities t
    org-ellipsis "…")
-  :hook (org-mode))
+
+  ;; bigger checkboxes
+  (setq org-modern-checkbox '((88 . "")
+			      (45 .
+				  #("–" 0 2
+				    (composition
+				     ((2)))))
+			      (32 . "")))
+
+  ;; Agenda styling
+  (setq org-agenda-tags-column 0
+	org-agenda-block-separator ?─
+	org-agenda-time-grid
+	'((daily today require-timed)
+	  (800 1000 1200 1400 1600 1800 2000)
+	  " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+	org-agenda-current-time-string
+	" now ─────────────────────────────────────────────────")
+  ;;(org-mode)
+  :hook
+  ((org-mode . org-modern-mode)
+   (org-agenda-finalize . org-modern-agenda)))
 
 ;; (global-org-modern-mode))
-
-;; Agenda styling
-;; org-agenda-tags-column 0
-;; org-agenda-block-separator ?─
-;; org-agenda-time-grid
-;; '((daily today require-timed)
-;;   (800 1000 1200 1400 1600 1800 2000)
-;;   " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"))
-;; org-agenda-current-time-string
-;; "⭠ now ─────────────────────────────────────────────────")
