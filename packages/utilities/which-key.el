bcba627e6e06f145f594ec2030c6d18a7ca1e6d3
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2022-12-23 13:05:03 +0100

    New package which-key added and with initial configuration

---
+++ packages/utilities/which-key.el
@@ -0,0 +1,31 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;             WHICH KEY              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; which-key helper
+(use-package which-key
+  :defer t
+  :general
+  (:keymaps 'which-key-mode-map
+	    ;; with bindings starting wit C-x, use SPC to cycle
+	    "C-x SPC" 'which-key-show-next-page-cycle)
+  :init
+  (which-key-mode)
+  :config
+  ;; Allow C-h to trigger which-key before it is done automatically
+  (setq which-key-show-early-on-C-h t)
+
+  (setq which-key-use-C-h-commands nil)
+
+  ;; control where which-key appears (bottom or minibuffer)
+  (which-key-setup-side-window-bottom)
+  ;; (minibuffer-key-setup-minibuffer)
+
+  ;; default
+  ;; (setq which-key-sort-order 'which-key-key-order)
+  ;; same as default, except all keys from local maps shown first
+  (setq which-key-sort-order 'which-key-local-then-key-order)
+
+  ;; make sure which-key doesn't show normally but refreshes quickly after it is triggered
+  (setq which-key-idle-delay 10000)
+  (setq which-key-idle-secondary-delay 0.05))
