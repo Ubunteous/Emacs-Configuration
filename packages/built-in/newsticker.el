6e9333a0b0ec45919704f20f6fec7c24685a0214
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-01-12 19:46:34 +0000

    Smaller rss feed and mail packages (less useful)

---
+++ packages/built-in/newsticker.el
@@ -0,0 +1,17 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;             NEWSTICKER             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package newsticker
+  :defer t
+  :straight (:type built-in)
+  :general
+  ("C-c w" 'newsticker-treeview)
+  :init
+  (setq newsticker-url-list-defaults nil) ;; optionally add nil 3600 after an url
+  (setq newsticker-url-list '(("Mastering emacs" "http://www.masteringemacs.org/feed")
+			      ("r/emacs" "https://www.reddit.com/r/emacs.rss")))
+  :config
+  (setq newsticker-retrieval-method 'extern))
+;; newsticker-frontend treeview
+;;     (setq newsticker-frontend 'newsticker-plainview)
---
+++ packages/built-in/smtpmail.el
@@ -0,0 +1,52 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;              SMTPMAIL              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; gpg --gen-key
+;; gpg --list-secret-keys
+
+;; gpg --full-generate-key
+
+;; may require 'gnutls-bin' in Debian/Ubuntu
+
+(use-package smtpmail
+  :defer t
+  :straight (:type built-in)
+  :config
+  ;; debug issues with authinfo
+  (setq auth-source-debug t)
+
+  (setq user-mail-address "pierredemetz999@gmail.com"
+	user-full-name  "Pierre")
+
+  ;; report problems with the smtp server
+  (setq smtpmail-debug-info t)
+
+  ;; (setq message-send-mail-function 'message-smtpmail-send-it)
+  (setq message-send-mail-function 'smtpmail-send-it
+	starttls-use-gnutls t
+	smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+	smtpmail-authx-credentials
+	'(("smtp.gmail.com" 587 "pierredemetz999@gmail.com" nil))
+	smtpmail-default-smtp-server "smtp.gmail.com"
+	smtpmail-smtp-server "smtp.gmail.com"
+	smtpmail-smtp-service 587)
+
+  ;; tell message-mode how to send mail
+  (setq message-send-mail-function 'smtpmail-send-it)
+  ;; if our mail server lives at smtp.example.org; if you have a local
+  ;; mail-server, simply use 'localhost' here.
+  ;; (setq smtpmail-smtp-server "smtp.example.org")
+  (setq smtpmail-smtp-server "smtp.gmail.com")
+
+  ;; add Cc and Bcc headers to the message buffer
+  ;; (setq message-default-mail-headers "Cc: \nBcc: \n")
+
+  ;; postponed message is put in the following draft directory
+  ;; (setq message-auto-save-directory "~/mail/draft")
+
+  ;; change the directory to store the sent mail
+  ;; (setq message-directory "~/mail/")
+
+  ;; don't keep message buffers around
+  (setq message-kill-buffer-on-exit t))
