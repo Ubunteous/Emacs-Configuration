969062091a0363220dd246cfe44d10b9e725b6cd
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-01-12 19:52:39 +0000

    Improved header inserter to determine comment-char

--- packages/perso/header.el
+++ packages/perso/header.el
@@ -3,19 +3,24 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun insert-header ()
-  "insert a nice header with the current buffer name at the beginning of said buffer"
+  "Insert a nice header with the current buffer name at the beginning of said buffer."
   (interactive)
 
-  (setq border ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
-  (setq name (upcase (file-name-sans-extension (buffer-name))))
+  (if comment-start
+      (let* ((comment-char (string-to-char comment-start))
+	     (border (make-string 40 comment-char))
+	     (two-sep (make-string 2 comment-char))
+	     (name (upcase (file-name-sans-extension (buffer-name))))
+	     (len-name (length name))
+	     (nb-spaces (/ (- (length border) len-name 4) 2))
+	     (white-insert (make-string nb-spaces ? )))
 
-  (setq len-name (length name))
-  (setq nb-spaces (/ (- (length border) len-name 4) 2))
-  (setq white-insert (make-string nb-spaces ? ))
+	;; go to the beginning of the buffer
+	;; (goto-char (point-min)) ;; (goto-char 1) works as well
+	
+	(insert (concat border "\n"
+			two-sep white-insert (if (eq 0 (% len-name 2))  "" " ") name white-insert two-sep "\n"
+			border "\n\n")))
 
-  (goto-char (point-min))
-  
-  (insert (format "%s\n;;%s;;\n%s\n\n"
-		  border
-		  (concat white-insert (if (eq 0 (% len-name 2))  "" " ") name white-insert)
-		  border)))
+    ;; Some modes can't support this function.
+    (message "Can't place a header here. The current mode does not have a comment-start character" )))
