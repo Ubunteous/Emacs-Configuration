5b8c70be2bbda28d22b21d03d2bb98540e604239
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-01-12 19:54:23 +0000

    Corfu cycle threshold reduced

--- packages/composite/corfu.el
+++ packages/composite/corfu.el
@@ -44,8 +44,8 @@
 (use-package emacs
   :straight (:type built-in)
   :config
-  ;; TAB cycle if there are only few candidates
-  (setq completion-cycle-threshold 3)
+  ;; TAB cycle if there are only few candidates (in this case, 2)
+  (setq completion-cycle-threshold 2)
   ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
   ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
   ;; (setq read-extended-command-predicate
