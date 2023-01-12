129db0fa76f3f7be576a722acd8bf6889a4cbce1
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-01-12 19:53:27 +0000

    Agenda files added

--- custom/customize.el
+++ custom/customize.el
@@ -17,7 +17,7 @@
    '("833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "78b52dbcfc163b4f85c4b8fc3c415f721d49f250708bce73a5a448798ed0a786" "828657dee73e7add633117164b41794e06971d65ece73c085c31091c55439fa7" "c8b83e7692e77f3e2e46c08177b673da6e41b307805cd1982da9e2ea2e90e6d7" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" default))
  '(initial-scratch-message ";; This is where the fun begins")
  '(org-agenda-files
-   '("~/org/emacs/agenda.org" "/home/ubunteous/Desktop/Alter/Alter.org" "/home/ubunteous/Desktop/Latex/Job/Contacts.org"))
+   '("/home/ubunteous/org/emacs/agenda.org" "/home/ubunteous/Desktop/Latex/Job/Contacts.org"))
  '(prescient-save-file "~/.emacs.d/files/prescient/prescient-save.el" t)
  '(recentf-save-file "~/.emacs.d/files/recentf/recentf")
  '(safe-local-variable-values
