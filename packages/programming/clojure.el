5d6bf753f7056124d6dc3325523519e416ab9ef2
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-01-30 15:19:54 +0000

    Clojure-mode and cider set up for future use

---
+++ packages/programming/clojure.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;               CLOJURE              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package clojure-mode
+  :defer t
+  :mode "\\.clj\\'")
+
+(use-package cider
+  :defer t
+  :mode "\\.clj\\'")
