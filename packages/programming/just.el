de85e806dd9ef85756d06c10f253509bab069cff
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-01-23 18:12:00 +0100

    just-mode and justl added to program justfiles

---
+++ packages/programming/just.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                JUST                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; just is a command runner like makefile
+(use-package just-mode
+  :defer t
+  :mode "justfile\\'")
+
+(use-package justl
+  :defer t)
