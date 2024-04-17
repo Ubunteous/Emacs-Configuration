f98d55a4ac6bf5ff91304fb6ebcc7a91d79dc7d7
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-04-17 19:43:16 +0200

    New simple elpy configuration

---
+++ packages/programming/python/elpy.el
@@ -0,0 +1,8 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                ELPY                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package elpy
+  :ensure t
+  :init (elpy-enable)
+  :hook python-ts-mode)
