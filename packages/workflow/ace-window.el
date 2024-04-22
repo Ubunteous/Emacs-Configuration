c02a3f7b0f898348fd43ec2d3d404d45f83905c8
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-04-22 20:58:24 +0200

    ace bindings changed to accommodate how row mods

--- packages/workflow/ace-window.el
+++ packages/workflow/ace-window.el
@@ -5,13 +5,18 @@
 (use-package ace-window
   :defer t
   :general
-  ("M-o" 'ace-window)
+  ("C-x o" 'ace-window
+   "M-o" 'ace-window)
   :config
   ;; azerty friendly list of keys
   ;; (setq aw-keys '(?& ?é ?\" ?' ?\( ?- ?è ?_ ?ç ?à))
-
+  
   ;; azerty home row to change window
   ;; note: j words as per usual if there are less
   ;; than 7 windows. w will add as a replacement if needed
-  (setq aw-keys '(?q ?s ?d ?f ?g ?h ?j ?k ?l))
+  ;; (setq aw-keys '(?q ?s ?d ?f ?g ?h ?j ?k ?l))
+
+  ;; colemak friendly keys
+  (setq aw-keys '(?a ?r ?s ?t ?g ?m ?n ?e ?i))
+  
   (add-to-list 'aw-dispatch-alist '(?w aw-switch-buffer-in-window "Select Buffer")))
