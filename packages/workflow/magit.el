b9f3fb64ec0ceb831229dab4f4f8443c915969d6
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-04-22 20:57:38 +0200

    Failed attempt to modify the modeline for magit

--- packages/workflow/magit.el
+++ packages/workflow/magit.el
@@ -9,4 +9,19 @@
 (use-package magit
   :defer t
   :config
+  ;; ;; updates modeline with branch. higher cost than hook
+  ;; (setq auto-revert-check-vc-info t
+  ;;       auto-revert-interval 10)
+
+
   (setq magit-remote-add-set-remote.pushDefault nil))
+
+
+;; (defadvice vc-git-mode-line-string (after plus-minus (file) compile activate)
+;;   (setq ad-return-value
+;; 	(concat ad-return-value
+;; 		(let ((plus-minus (vc-git--run-command-string
+;; 				   file "diff" "--numstat" "--")))
+;; 		  (and plus-minus
+;; 		       (string-match "^\\([0-9]+\\)\t\\([0-9]+\\)\t" plus-minus)
+;; 		       (format " +%s-%s" (match-string 1 plus-minus) (match-string 2 plus-minus)))))))
