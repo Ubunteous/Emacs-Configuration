28f1264a78d6017278045e46b73adc3391e6c86e
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-05-30 18:41:36 +0200

    Inf elixir package added for repl based development

--- packages/programming/elixir.el
+++ packages/programming/elixir.el
@@ -5,6 +5,17 @@
 (use-package elixir-mode
   :defer t)
 
+;; not tested yet. elixir repl IEx
+;; (use-package inf-elixir
+;;   :defer t
+;;   :general
+;;   ("C-c i i" 'inf-elixir
+;;    "C-c i p" 'inf-elixir-project
+;;    "C-c i l" 'inf-elixir-send-line
+;;    "C-c i r" 'inf-elixir-send-region
+;;    "C-c i b" 'inf-elixir-send-buffer
+;;    "C-c i R" 'inf-elixir-reload-module))
+
 ;; ;; too recent
 ;; (use-package elixir-ts-mode
 ;;   :defer t)
