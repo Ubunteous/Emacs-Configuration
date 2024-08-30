8b8db9aa981ea75830ef40c6583de1c5f111507e
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-08-30 18:53:03 +0200

    Swiper brought back with meow compatible binding

--- packages/composite/minimal-swiper.el
+++ packages/composite/minimal-swiper.el
@@ -9,14 +9,16 @@
   :defer t
   :general
   ("C-s" 'swiper-isearch)
+  ("C-c s" 'swiper-isearch)
   :custom-face
   ;;there are 4 different  background-match-face and match-face
-  (swiper-background-match-face-1 ((nil (:background "light sea green"))))
-  (swiper-background-match-face-2 ((nil (:background "light sea green"))))
-  (swiper-background-match-face-3 ((nil (:background "light sea green"))))
-  (swiper-background-match-face-4 ((nil (:background "light sea green"))))
-  (swiper-match-face-1 ((nil (:background "tomato"))))
-  (swiper-match-face-2 ((nil (:background "light sea green"))))
-  (swiper-match-face-3 ((nil (:background "light sea green"))))
-  (swiper-match-face-4 ((nil (:background "light sea green"))))
-  (swiper-line-face ((nil (:foreground nil)))))
+  (swiper-background-match-face-1 ((t (:background "light sea green"))))
+  (swiper-background-match-face-2 ((t (:background "light sea green"))))
+  (swiper-background-match-face-3 ((t (:background "light sea green"))))
+  (swiper-background-match-face-4 ((t (:background "light sea green"))))
+  (swiper-match-face-1 ((t (:background "tomato"))))
+  (swiper-match-face-2 ((t (:background "light sea green"))))
+  (swiper-match-face-3 ((t (:background "light sea green"))))
+  (swiper-match-face-4 ((t (:background "light sea green"))))
+  (swiper-line-face ((t (:foreground "unspecified"))))
+  )
