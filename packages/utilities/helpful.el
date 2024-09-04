b221e940a09809266f19c4cc77c6d380cae62d70
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-09-05 01:33:07 +0200

    Dedicated frame and navigation bindings for helpful

--- packages/utilities/helpful.el
+++ packages/utilities/helpful.el
@@ -4,6 +4,10 @@
 
 (use-package helpful
   :defer t
+  :config
+  ;; always open help in the same space/window
+  (add-to-list 'display-buffer-alist
+	       '("*Help*" display-buffer-reuse-mode-window))
   :general
   ;; Note that the built-in `describe-function' includes both functions
   ;; and macros. `helpful-function' is functions only, so we provide
@@ -15,16 +19,21 @@
   ;; Lookup the current symbol at point. C-c C-d is a common keybinding
   ;; for this in lisp modes.
   ("C-c C-d" #'helpful-at-point)
+  ("C-c C-c" #'helpful-at-point)
 
   ;; Look up *F*unctions (excludes macros).
-  
+
   ;; By default, C-h F is bound to `Info-goto-emacs-command-node'. Helpful
   ;; already links to the manual, if a function is referenced there.
   ("C-h F" #'helpful-function)
 
   ;; Look up *C*ommands.
-  
+
   ;; By default, C-h C is bound to describe `describe-coding-system'. I
   ;; don't find this very useful, but it's frequently useful to only
   ;; look at interactive functions.
-  ("C-h C" #'helpful-command))
+  ("C-h C" #'helpful-command)
+
+  ;; isearch within helpful
+  (:keymaps 'helpful-mode-map
+	    "s" 'swiper-isearch))
