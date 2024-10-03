1246f42e2042bcfb4d6d1a67e20d0478014417aa
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-10-03 19:31:32 +0200

    New ob packages (nix, sql, html) for literate org-mode

---
+++ packages/org/ob-html.el
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;               OB-HTML              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package ob-html
+  :defer t
+  :ensure (ob-html :type git :host github :repo "misohena/ob-html")
+  ;; :config
+  ;; (with-eval-after-load "org"
+  ;;   (require 'ob-html)
+  ;;   (org-babel-html-enable-open-src-block-result-temporary))
+  )
---
+++ packages/org/ob-sql.el
@@ -0,0 +1,13 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;               OB-SQL               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package ob-sql-mode
+  :defer t
+  ;; :config
+  ;; (require 'ob-sql-mode)
+  ;; ;; no prompt to evaluate sql block
+  ;; (setq org-confirm-babel-evaluate
+  ;; 	(lambda (lang body)
+  ;;         (not (string= lang "sql-mode"))))
+  )
