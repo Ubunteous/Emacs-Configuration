f04833dbaafa0eb818719698a08c437eaa16abe5
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-11-23 19:07:34 +0100

    New janet package

---
+++ packages/programming/janet.el
@@ -0,0 +1,33 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                JANET               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; used by org-babel
+(use-package janet-mode
+  :defer t)
+
+(use-package janet-ts-mode
+  :defer t
+  :ensure (:host github
+		 :repo "sogaiu/janet-ts-mode"
+		 :files ("*.el"))
+  :mode "\\.janet\\'"
+  :general (:keymaps 'janet-ts-mode-map
+		     "C-c s" '(lambda () (interactive)
+				(if (get-buffer "*ajrepl-repl*")
+				    (ajrepl-send-buffer)
+				  (ajrepl)))))
+
+(use-package ajrepl
+  :defer t
+  :ensure (:host github
+		 :repo "sogaiu/ajrepl"
+		 :files ("*.el" "ajrepl"))
+  :hook (janet-ts-mode . ajrepl-interaction-mode))
+
+;; need to add janet to flycheck-compile list
+;; (use-package flycheck-janet
+;;   :defer t
+;;   :ensure (:host github
+;; 		 :repo "sogaiu/flycheck-janet"
+;; 		 :files ("*.el")))
