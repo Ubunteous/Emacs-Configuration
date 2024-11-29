05c957f8fd6ba5dc2996ea76e17969f430ecd6ac
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-11-29 21:58:46 +0100

    Initial C# setup

---
+++ packages/org/ob-csharp.el
@@ -0,0 +1,7 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                OB-C#               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package  ob-csharp
+  :defer t
+  :ensure (ob-csharp :type git :host github :repo "samwdp/ob-csharp"))
---
+++ packages/programming/c#.el
@@ -0,0 +1,20 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                 C#                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; missing bindings to build and run. also test and debug
+
+;; (use-package sharper
+;;   :defer t
+;;   :general ("C-c #" 'sharper-main-transient))
+
+;; (use-package csproj-mode
+;;   :defer t
+;;   :ensure (name :type git :host github :repo "omajid/csproj-mode")
+;;   :mode "\\.csproj\\'")
+
+;; (use-package dotnet
+;;   :defer t
+;;   ;; :config
+;;   ;; (setq dotnet-mode-keymap-prefix (kbd "<ADD YOUR PREFIX HERE>"))
+;;   :hook csharp-ts-mode)
