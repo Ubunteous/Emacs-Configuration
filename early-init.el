81e0fb02e33a8cf50eceed39f7e7d681928ea1ec
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-12-01 22:45:40 +0100

    Switching eln-dir

--- early-init.el
+++ early-init.el
@@ -5,6 +5,16 @@
 ;; straight.el replaces package.el
 (setq package-enable-at-startup nil)
 
+;; ;; Set eln-cache dir (does not work)
+;; (when (boundp 'native-comp-eln-load-path)
+;;   (startup-redirect-eln-cache (expand-file-name "files/eln-cache/" user-emacs-directory)))
+
+;; set eln-cache dir (new attempt)
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name  "files/eln-cache/" user-emacs-directory))))
+
 ;; More speed improvements: https://github.com/hlissner/doom-emacs/blob/develop/docs/faq.org#how-does-doom-start-up-so-quickly
 
 ;;; early-init.el ends here
