3e56ec08b6f98ec249cbb140ab0f293748349275
Ubunteous-win <46612154+Ubunteous@users.noreply.github.com>
2024-12-13 10:37:21 +0100

    Final refactoring for dired-count-lines

--- packages/perso/dired-count-lines.el
+++ packages/perso/dired-count-lines.el
@@ -2,36 +2,28 @@
 ;;          DIRED-COUNT-LINES         ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'subr-x)
-(cl-defun dired-count-lines ()
+(defun dired-count-lines ()
   "Count the line number of each marked readable regular file in the dired buffer."
   (interactive)
 
-  (let ((init 0)
-	(reduction #'+)
-	(fun '(count-lines (point-min) (point-max))))
-
-    (save-excursion
-      (dired-map-over-marks
-       (let ((file (dired-file-name-at-point)))
-	 (when (and (file-regular-p file)
-                    (file-readable-p file))
-	   (with-temp-buffer
-             (insert-file-contents file)
-             (setq init
-		   (funcall reduction
-			    (eval fun)
-			    init)))))
-       nil))
-    (when (called-interactively-p 'any)
-      (message "Lines: %s" init))
-    init))
-
-;; (general-define-key
-;;  :keymaps 'dired-mode-map
-;;  "/" 'dired-count-lines)
+  (save-excursion
+    (dired-map-over-marks
+     (let ((file (dired-file-name-at-point)))
+       (when (and (file-regular-p file)
+		  (file-readable-p file))
+	 (with-temp-buffer
+	   (insert-file-contents file)
+	   (setq init (funcall
+		       #'+
+		       (count-lines (point-min) (point-max)))))))
+     nil))
+  
+  (when (called-interactively-p 'any)
+    (message "Lines: %s" init))
+  init)
 
 ;; shadows dired-prev/next-dirline but I don't need them with meow
+;; alternatively, / is a free binding
 (general-define-key
  :keymaps 'dired-mode-map
  "<" 'dired-count-lines)
