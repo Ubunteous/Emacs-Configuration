5fda2fec49f679aaa7b59f24d7c1770e761a0dee
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-01-06 21:15:29 +0100

    Adding markdown support

---
+++ packages/programming/markdown.el
@@ -0,0 +1,7 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;              MARKDOWN              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package markdown-mode
+  :defer t
+  :hook (markdown-mode . display-line-numbers-mode))
