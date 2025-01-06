0c1c21b834fe02bb3e05caced897f5a6663dae0f
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-01-06 21:14:14 +0100

    Smart insert (semi)colon

---
+++ packages/perso/smart-insert.el
@@ -0,0 +1,50 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;            SMART-INSERT            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun smart-insert-semicolon () (interactive)
+       """Checks the last char of a line to figure out if
+a semicolon needs to be added before inserting a newline"""
+       (when
+	   (and
+	    ;; current line is not a comment
+	    (save-excursion
+	      (call-interactively 'back-to-indentation)
+	      (and
+	       (not (eobp)) ;; check char-after exists
+	       (not (= ?/ (char-after)))))
+	    
+	    ;; cursor is at the end of the line
+	    (eolp)
+	    
+	    ;; last character is a word or )/]
+	    (or
+	     (= ?w (char-syntax (char-before))) ;; word
+	     (eq ?\) (char-before)) ;; )
+	     (eq ?\] (char-before))))  ;; ]
+
+	 (insert ";"))
+       (newline))
+
+(defun smart-insert-colon () (interactive)
+       """Checks the first and last char of a line to figure out if
+a colon needs to be added before inserting a space"""
+       (when
+	   (and
+	    ;; current line is not a comment/selector (starts with whitespace/indent)
+	    (save-excursion
+	      (call-interactively 'beginning-of-line)
+	      (and
+	       (not (eobp)) ;; check char-after exists
+	       (or
+		(= ?  (char-after))
+		(= ?\C-i  (char-after)))))
+	    
+	    ;; cursor is at the end of the line
+	    (eolp)
+	    
+	    ;; last character is a word
+	    (= ?w (char-syntax (char-before))))
+
+	 (insert ": ")))
+
