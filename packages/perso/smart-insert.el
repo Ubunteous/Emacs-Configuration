19a22eed53e47d5b197b5f9df0f54cb6fea9fb69
Ubunteous-win <46612154+Ubunteous@users.noreply.github.com>
2025-01-23 15:33:39 +0100

    Fix smart-insert documentation

--- packages/perso/smart-insert.el
+++ packages/perso/smart-insert.el
@@ -2,48 +2,50 @@
 ;;            SMART-INSERT            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun smart-insert-semicolon () (interactive)
-       """Checks the last char of a line to figure out if
-a semicolon needs to be added before inserting a newline"""
-       (when
-	   (and
-	    ;; current line is not a comment
-	    (save-excursion
-	      (call-interactively 'back-to-indentation)
-	      (and
-	       (not (eobp)) ;; check char-after exists
-	       (not (= ?/ (char-after)))))
-	    
-	    ;; cursor is at the end of the line
-	    (eolp)
-	    
-	    ;; last character is a word or a closing delimiter
-	    (or
-	     (= ?w (char-syntax (char-before))) ;; word
-	     (member (char-before) '(?\) ?\] ?\" ?\%))))
+(defun smart-insert-semicolon ()
+  "Checks the last char of a line to figure out if
+a semicolon needs to be added before inserting a newline"
+  (interactive)
+  (when
+      (and
+       ;; current line is not a comment
+       (save-excursion
+	 (call-interactively 'back-to-indentation)
+	 (and
+	  (not (eobp)) ;; check char-after exists
+	  (not (= ?/ (char-after)))))
+       
+       ;; cursor is at the end of the line
+       (eolp)
+       
+       ;; last character is a word or a closing delimiter
+       (or
+	(= ?w (char-syntax (char-before))) ;; word
+	(member (char-before) '(?\) ?\] ?\" ?\%))))
 
-	 (insert ";"))
-       (newline-and-indent))
+    (insert ";"))
+  (newline-and-indent))
 
-(defun smart-insert-colon () (interactive)
-       """Checks the first and last char of a line to figure out if
-a colon needs to be added before inserting a space"""
-       (when
-	   (and
-	    ;; current line is not a comment/selector (starts with whitespace/indent)
-	    (save-excursion
-	      (call-interactively 'beginning-of-line)
-	      (and
-	       (not (eobp)) ;; check char-after exists
-	       (or
-		(= ?  (char-after))
-		(= ?\C-i  (char-after)))))
-	    
-	    ;; cursor is at the end of the line
-	    ;; (eolp)
-	    
-	    ;; last character is a word
-	    (= ?w (char-syntax (char-before))))
+(defun smart-insert-colon ()
+  "Checks the first and last char of a line to figure out if
+a colon needs to be added before inserting a space"
+  (interactive)
+  (when
+      (and
+       ;; current line is not a comment/selector (starts with whitespace/indent)
+       (save-excursion
+	 (call-interactively 'beginning-of-line)
+	 (and
+	  (not (eobp)) ;; check char-after exists
+	  (or
+	   (= ?  (char-after))
+	   (= ?\C-i  (char-after)))))
+       
+       ;; cursor is at the end of the line
+       ;; (eolp)
+       
+       ;; last character is a word
+       (= ?w (char-syntax (char-before))))
 
-	 (insert ":"))
-       (insert " "))
+    (insert ":"))
+  (insert " "))
