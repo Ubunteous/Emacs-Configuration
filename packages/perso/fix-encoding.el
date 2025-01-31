1783a93c8ea97e0597a37bc688e7d57ab2434068
Ubunteous-win <46612154+Ubunteous@users.noreply.github.com>
2025-01-31 10:41:08 +0100

    New character for fix-encoding

--- packages/perso/fix-encoding.el
+++ packages/perso/fix-encoding.el
@@ -5,10 +5,10 @@
 (defun fix-encoding ()
   "Replaces incorrect characters by their UTF-8 equivalent."
   (interactive)
-
   (let ((pmin (point-min))
 	(pmax (point-max)))
     (replace-string "Ã¢" "â" nil pmin pmax)
+    (replace-string "�" "â" nil pmin pmax)
     (replace-string "Ã " "à" nil pmin pmax)
     (replace-string "�" "à" nil pmin pmax)
     (replace-string "Ã§" "ç" nil pmin pmax)
@@ -21,6 +21,7 @@
     (replace-string "�" "è" nil pmin pmax)
     (replace-string "Ã®" "î" nil pmin pmax)
     (replace-string "�" "î" nil pmin pmax)
-    (replace-string "ô" "ô" nil pmin pmax)
+    (replace-string "Ã´" "ô" nil pmin pmax)
     (replace-string "Ã¹" "ù" nil pmin pmax)
+    (replace-string "�" "ù" nil pmin pmax)
     (replace-string "û" "û" nil pmin pmax)))
