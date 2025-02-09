9c794dae2a28b657bfb4f8354a3debdfc31ae810
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-02-09 18:17:26 +0100

    Initial smartparens setup

--- packages/utilities/smartparens.el
+++ packages/utilities/smartparens.el
@@ -7,5 +7,23 @@
   :defer t
   :config
   (require 'smartparens-config)
-;; :hook (prog-mode . smartparens-strict-mode))
+
+  (transient-define-prefix ts-smartparens ()
+    "Prefix with groups in a grid-like arrangement for smartparens."
+
+    [:description
+     "SMARTPARENS\n" ; must use slot or macro is confused
+     ["Main Commands" ; note, no newline
+      ("s" "Slurp" sp-forward-slurp-sexp)
+      ("C-s" "Back Slurp" sp-backward-slurp-sexp)
+      ("b" "Barf" sp-forward-barf-sexp)
+      ("C-b" "Barf" sp-backward-barf-sexp)
+      ""
+      ("a" "placeholder" newline)] ; note, no newline
+
+     ["Motion\n"
+      ("b" "placeholder" newline)]])
+  :general (:keymaps 'prog-mode-map
+		     "C-r" 'ts-smartparens) ;; shadows isearch-backwards
+  ;; smartparens-strict-mode to disable invalid pairing
   :hook prog-mode)
