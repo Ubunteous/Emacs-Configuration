81beec2da102835b2e1df11e81e30775e3af00b2
Ubunteous-win <46612154+Ubunteous@users.noreply.github.com>
2025-02-28 11:18:34 +0100

    Initial config for completion preview (30.1)

--- packages/built-in/completion.el
+++ packages/built-in/completion.el
@@ -2,6 +2,36 @@
 ;;             COMPLETION             ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package completion-preview
+  :ensure nil
+  :defer t
+  :config
+  (setq completion-preview-completion-styles '(orderless-first-prefix))
+
+  ;; (setq completion-preview-sort-function 'minibuffer--sort-by-length-alpha)
+  
+  ;; (setq completion-preview-minimum-symbol-length 3) ;; default
+  (setq completion-preview-idle-delay 2) ;; in seconds
+
+  ;; commands which triger completion-preview
+  ;; (setq completion-preview-command '(insert-char delete-backward-char backward-delete-char-untabify analyze-text-conversion completion-preview-complete))
+
+  ;; (add-to-list 'completion-preview-command
+  ;; 			   '(org-self-insert-command
+  ;; 				 org-delete-backward-char
+  ;; 				 self-insert-command
+  ;; 				 meow-backward-char)
+  :general
+  ;; "M-i" 'completion-preview-complete
+  (:keymaps 'completion-preview-active-mode-map
+			"<escape>" 'completion-preview-hide
+
+			;; "RET" 'newline
+			;; "TAB" 'completion-preview-insert/complete
+			"C-n" 'completion-preview-next-candidate
+			"C-p" 'completion-preview-prev-candidate)
+  :hook emacs-lisp-mode)
+
 (use-package hippie
   :defer t
   :ensure nil
--- packages/composite/orderless.el
+++ packages/composite/orderless.el
@@ -9,7 +9,7 @@
   ;; (completion-category-overrides '((file (styles basic partial-completion)))))
   :config
   (setq completion-styles
-	'(partial-completion substring flex))
+		'(partial-completion substring flex))
   ;; '(partial-completion substring first-initialism flex))
   ;; '(partial-completion substring initials flex))
 
@@ -18,7 +18,11 @@
   ;; 	  (buffer (styles . (basic partial-completion substring)))
   ;; 	  ;; (project-file (styles . (partial-completion substring)))
   ;; 	  (info-menu (styles . (substring)))))
-  )
+  
+  (defun first-prefix-dispatch (pattern index _total)
+	(if (= index 0) 'orderless-literal-prefix))
+  (orderless-define-completion-style orderless-first-prefix
+	(orderless-style-dispatchers '(first-prefix-dispatch orderless-affix-dispatch))))
 
 ;; (defun first-initialism (pattern index _total)
 ;;   (if (= index 0) 'orderless-initialism))
