e76c65702ca7fa805cc101afd496c194de5c4e21
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-06-10 22:09:37 +0200

    New highlighted keywords

--- packages/passive-utils/hl-todo.el
+++ packages/passive-utils/hl-todo.el
@@ -7,9 +7,29 @@
   :hook (prog-mode . hl-todo-mode)
   :config
   (setq hl-todo-keyword-faces
-	'(("TODO"   . "#FF0000")
+		'(("TODO"   . "#FF0000")
           ("FIXME"  . "#FF0000")
           ("DEBUG"  . "#A020F0")
-          ("GOTCHA" . "#FF4500")
+		  ("GOTCHA" . "#FF4500")
+		  ("DEPRECATED" . "#BEBEBE")
+		  ("PROGRESS" . "#FFA500")
           ("STUB"   . "#1E90FF")
-	  ("DONE"   . "#1EFF90"))))
+		  ("DONE"   . "#1EFF90"))))
+
+;; Other keywords from KDE syntax highlighting
+;; ALERT
+;; ATTENTION
+;; DANGER
+;; HACK
+;; SECURITY
+
+;; BUG
+;; FIXME
+;; DEPRECATED
+;; TASK
+;; TODO
+;; TBD
+;; WARNING
+;; CAUTION
+;; NOLINT
+;; NOQA
