79bac113710df7ace1737f41daaa630dd92b8237
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-07-09 22:12:53 +0200

    Fix haskell mode

--- packages/programming/haskell.el
+++ packages/programming/haskell.el
@@ -4,9 +4,10 @@
 
 (use-package haskell-mode
   :mode "\\.hs\\'"
-  :hook ((haskell-mode . 'turn-on-haskell-indent)
-	 (haskell-mode . (lambda () (aggressive-indent-mode -1)))
-	 (haskell-mode . (lambda () (electric-indent-mode)))))
+  :hook (haskell-mode . (lambda ()
+						  (turn-on-haskell-indent)
+						  (aggressive-indent-mode -1)
+						  (electric-indent-mode))))
 
 ;; (use-package nix-haskell-mode
 ;;   :hook (haskell-mode . nix-haskell-mode))
