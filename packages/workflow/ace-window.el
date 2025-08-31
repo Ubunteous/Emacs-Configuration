8077251f635350f6419eff4c6c3706b3ed52273b
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-08-31 21:04:03 +0200

    smarter ace-window

--- packages/workflow/ace-window.el
+++ packages/workflow/ace-window.el
@@ -5,7 +5,7 @@
 (use-package ace-window
   :defer t
   :general
-  ("C-x o" 'ace-window
+  ("C-x o" 'ace-window-dwim
    "M-o" 'ace-window)
   :config
   ;; azerty friendly list of keys
@@ -16,7 +16,25 @@
   ;; than 7 windows. w will add as a replacement if needed
   ;; (setq aw-keys '(?q ?s ?d ?f ?g ?h ?j ?k ?l))
 
+  (defun ace-window-dwim ()
+	(interactive)
+	;; smarter window management
+	(if (= (count-windows) 1)
+		(split-window-right)
+	  (ace-window nil))
+
+	;; (let ((nb-windows (count-windows)))
+	;;   (cond ((= nb-windows 1) (progn
+	;; 							(split-window-right)
+	;; 							(windmove-right)))
+	;; 		((= nb-windows 2) (progn
+	;; 							;; (windmove-right)
+	;; 							(split-window-below)
+	;; 							(windmove-down)))
+	;; 		(t (ace-window nil))))
+	)
+
   ;; colemak friendly keys
   (setq aw-keys '(?a ?r ?s ?t ?g ?m ?n ?e ?i))
-  
+
   (add-to-list 'aw-dispatch-alist '(?w aw-switch-buffer-in-window "Select Buffer")))
