c71f62b29b0a7c2b94c5b705be34204549728544
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-08-31 21:05:23 +0200

    clean duplicates

--- packages/built-in/fly.el
+++ packages/built-in/fly.el
@@ -93,14 +93,6 @@
 ;; (add-to-list 'flycheck-checkers 'python-mypy t)
 ;; (flycheck-add-next-checker 'python-pylint 'python-mypy t))
 
-(use-package flyspell-correct
-  :after flyspell
-  :general
-  :general (:keymaps 'flyspell-mode-map
-					 ;; shadows ispell-word
-					 ;; it can show options as a drop down list with popup package
-					 "M-$" 'flyspell-correct-wrapper))
-
 ;; error with after-load-theme-hook
 ;; (use-package flycheck-overlay
 ;;   :defer t
