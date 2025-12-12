57d08dfb201602342973d9e64fc79544a39ddd82
Ubunteous-win <46612154+Ubunteous@users.noreply.github.com>
2025-12-12 16:34:31 +0100

    Improve isearch with new functions

--- packages/built-in/isearch.el
+++ packages/built-in/isearch.el
@@ -19,13 +19,43 @@
   ;; (setq isearch-hide-immediately nil) ;; for invisible matches
   ;; (setq-isearch-wrap-pause 'no-ding) ;; wrap if no match found
   ;; (setq isearch-yank-on-move t) ;; motion keys select til occurrence
+
+  (defun isearch-exit-other-end ()
+	"Exit isearch, but at the other end of the search string.
+This is useful when followed by an immediate kill."
+	(interactive)
+	(isearch-exit)
+	(goto-char isearch-other-end))
+
+  ;; (defun isearch-yank-symbol ()
+  ;; 	"*Put symbol at current point into search string."
+  ;; 	(interactive)
+  ;; 	(let ((sym (thing-at-point 'symbol)))
+  ;;     (if sym
+  ;;         (progn
+  ;; 			(setq isearch-regexp t
+  ;;                 isearch-string (concat "\\_<" (regexp-quote sym) "\\_>")
+  ;;                 isearch-message (mapconcat 'isearch-text-char-description isearch-string "")
+  ;;                 isearch-yank-flag t))
+  ;; 		(ding)))
+  ;; 	(isearch-search-and-update))
+
+  (defun isearch-consult ()
+    "Invoke `consult-line' from isearch."
+    (interactive)
+    (let ((query (if isearch-regexp
+                     isearch-string
+                   (regexp-quote isearch-string))))
+      (isearch-update-ring isearch-string isearch-regexp)
+      (let (search-nonincremental-instead)
+        (ignore-errors (isearch-done t t)))
+      (consult-line query)))
   :custom-face
   (isearch ((t (
 				:weight bold
 				;; :underline  (:color foreground-color :style line)
 				:foreground "light sea green"
-				:background "black"
-				))))
+				:background "black"))))
 
   (lazy-highlight ((t (:foreground "light sea green" :background "black"))))
   (isearch-fail ((t (:foreground "black"  :background "MediumPurple"))))
@@ -38,7 +68,10 @@
 			;; "<left>" 'isearch-ring-advance 
 			;; "<right>" 'isearch-forward
 			"<up>" 'isearch-repeat-backward
-			"<down>" 'isearch-repeat-forward))
+			"<down>" 'isearch-repeat-forward
+			"C-<return>" 'isearch-exit-other-end
+			"C-a" 'avy-isearch
+			"C-o" 'isearch-consult))
 
 
 ;; go to the end of a match when using isearch-backward
