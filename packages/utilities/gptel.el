8f78c77320e5fdd083637278053b237366b466a1
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-02-12 12:32:27 +0100

    Use gemini by default in gptel

--- packages/utilities/gptel.el
+++ packages/utilities/gptel.el
@@ -22,13 +22,13 @@
 (use-package gptel
   :defer t
   :config
-  ;; (setq gptel-model 'gemini-2.5-flash-lite
-  ;; 		gptel-backend
-  ;; 		(gptel-make-gemini "Gemini"
-  ;; 		  :key
-  ;; 		  (plist-get (car (auth-source-search
-  ;; 						   :host "googleapis.com"
-  ;; 						   :user "apikey"))
-  ;; 					 :secret)
-  ;; 		  :stream t))
-  )
+  (setq gptel--system-message "Respond concisely.")
+  (setq gptel-model 'gemini-2.5-flash-lite
+		gptel-backend
+		(gptel-make-gemini "Gemini"
+		  :key
+		  (plist-get (car (auth-source-search
+						   :host "googleapis.com"
+						   :user "apikey"))
+					 :secret)
+		  :stream t)))
