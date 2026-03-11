92c43c2cdb3a5c78e7f294dc74ed955ba608b008
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-03-11 12:31:15 +0100

    Add marginalia function to simplify variable view

--- packages/composite/marginalia.el
+++ packages/composite/marginalia.el
@@ -10,6 +10,19 @@
   :general
   ("M-A" 'marginalia-cycle)
   (:map 'minibuffer-local-map
-	"M-A" 'marginalia-cycle)
+		"M-A" 'marginalia-cycle)
   :init
-  (marginalia-mode))
+  (marginalia-mode)
+  :config
+  ;; marginalia uses functions for annotations. Use this one to simplify describe-variable
+  (defun marginalia-simplify-variable-description ()
+	;; Remove class/symbol in describe-variable. Not a toggle.
+	(interactive)
+	(defun marginalia-annotate-variable (cand)
+	  "Annotate variable CAND with its documentation string."
+	  (when-let* ((sym (intern-soft cand)))
+		(marginalia--fields
+		 ((or (documentation-property sym 'variable-documentation)
+			  (marginalia--definition-prefix sym))
+		  :truncate 1.0 :face 'marginalia-documentation)))))
+  )
