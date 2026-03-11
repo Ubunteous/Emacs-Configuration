e5046e53c9c67999d67ac3e6efdcaafae089947d
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-03-11 09:17:52 +0100

    Replace compile function by node-run in javascript

--- packages/programming/javascript.el
+++ packages/programming/javascript.el
@@ -9,36 +9,38 @@
   :defer t
   :general
   (:keymaps 'js2-minor-mode-map
-			"C-c C-c" 'recompile
+			"C-c C-c" 'node-run
 			"M-;" 'js2-jump-to-definition
 			"RET" 'smart-insert-semicolon)
-  
+
   ;; :mode ("\\.js\\'" . js2-mode) ;; major mode
   ;; :mode (("\\.js\\'" . js2-mode)
-  ;; 	 ("\\.jsx?\\'" . js2-jsx-mode))
+  ;;	 ("\\.jsx?\\'" . js2-jsx-mode))
   ;; as minor mode with js-mode for react
   ;; :interpreter (("node" . js2-mode)
-  ;; 		("node" . js2-jsx-mode))
+  ;;		("node" . js2-jsx-mode))
   :config
   (setq org-babel-js-cmd "node")
-  ;; (setq org-babel-js-cmd "bun")
-  (defun set-js-compile-command ()
-	(setq-local compile-command
-				(concat
-				 "node "
-				 (when buffer-file-name
-				   (shell-quote-argument
-					buffer-file-name
-					;; (file-name-sans-extension buffer-file-name)
-					)))))
+
+  (defun node-run ()
+	"Run Node on DIRECTORY (defaults to project root)."
+	(interactive)
+	(let* ((project-p (project-current nil))
+		   (default-file (if project-p
+							 (concat (project-root project-p) "src/index.js")
+						   (buffer-file-name)))
+		   (default-directory (if project-p
+								  (project-root project-p)
+								(file-name-directory default-file))))
+
+	  (compilation-start (concat "node " (shell-quote-argument (expand-file-name default-file t))))))
 
   (add-to-list 'compilation-error-regexp-alist 'node)
 
   ;; 1-file, 2-line, 3-column (groups delimited by \\(...\\))
   (add-to-list 'compilation-error-regexp-alist-alist
 			   '(node "\\(/[[:alpha:]/\s]+.js\\):\\([[:digit:]]+\\)" 1 2))
-  :hook (((js-mode js-ts-mode typescript-ts-mode) . js2-minor-mode)
-		 ((js-mode js-ts-mode typescript-ts-mode) . set-js-compile-command)))
+  :hook (((js-mode js-ts-mode typescript-ts-mode) . js2-minor-mode)))
 
 ;; ;; cannot be lazy loaded easily
 ;; (use-package js-comint
