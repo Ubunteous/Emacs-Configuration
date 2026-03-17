ce322fdbb3ce6f4bd39fb0c4cab3a2560c462a98
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-03-17 21:44:09 +0100

    Add cargo run function for rust

--- packages/programming/rust.el
+++ packages/programming/rust.el
@@ -8,10 +8,27 @@
   (setq rust-format-on-save t)
   (setq rust-mode-treesitter-derive t)
   :mode "\\.rs\\'"
+  :general
+  ("C-c s" 'cargo-run)
   :hook
   (rust-mode . (lambda ()
 				 (setq indent-tabs-mode nil)
 				 (prettify-symbols-mode))))
+
+(defun cargo-run ()
+  "Run Node on DIRECTORY (defaults to project root)."
+  (interactive)
+
+  (save-buffer)
+  (let* ((project-p (project-current nil))
+		 (default-file (if project-p
+						   (concat (project-root project-p) "src/main.rs")
+						 (buffer-file-name)))
+		 (default-directory (if project-p
+								(project-root project-p)
+							  (file-name-directory default-file))))
+
+	(compilation-start (concat "cargo run " (shell-quote-argument (expand-file-name default-file t))))))
 
 ;; (use-package flycheck-rust
 ;;   :defer t
