39c106a4de3f93f2d90131f0fb81df2338feeb68
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-03-18 09:36:23 +0100

    Add kotlin mode

---
+++ packages/programming/kotlin.el
@@ -0,0 +1,22 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;               KOTLIN               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package kotlin
+  :defer t
+  ;; :config
+  ;; (setq kotlin-command "kotlinc")
+  ;; (setq kotlin-mode-highlight-anchor t)
+  ;; (setq kotlin-mode-indent-nonempty-line-in-multiline-string nil)
+
+  ;; (setq kotlin-mode-fix-comment-close t
+  ;;		kotlin-mode-auto-close-multiline-comment t
+  ;;		kotlin-mode-break-line-before-comment-close t
+  ;;		kotlin-mode-prepend-asterisk-to-comment-line t
+  ;;		kotlin-mode-insert-space-after-asterisk-in-comment t)
+
+  ;; (setq kotlin-args-repl nil)
+  :mode "\\.rs\\'")
+
+;; (use-package kotlin-ts-mode
+;;   :defer t)
