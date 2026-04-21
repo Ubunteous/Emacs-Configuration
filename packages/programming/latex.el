a8a6203ba444886fb62396036402d85fcaa4332f
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-04-21 23:02:14 +0200

    Cleaning (latex.el)

--- packages/programming/latex.el
+++ packages/programming/latex.el
@@ -2,29 +2,8 @@
 ;;     PROGRAMMING LANGUAGE: LaTeX    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; (use-package reftex
-;;   :ensure nil
-;;   :defer t)
-
-;; (use-package auctex
-;;   :ensure (auctex :pre-build (("./autogen.sh")
-;;                               ("./configure"
-;;                                "--with-texmf-dir=$(dirname $(kpsexpand '$TEXMFHOME'))")
-;;                               ("make")))
-;;   :mode ("\\.tex\\'" . TeX-latex-mode)
-;;   :config
-;;   (load "latex.el" nil t t)
-;;   (load "preview-latex.el" nil t t)
-;;   (setq-default preview-scale 1.4
-;;                 preview-scale-function
-;;                 (lambda () (* (/ 10.0 (preview-document-pt)) preview-scale)))
-;;   (setq preview-auto-cache-preamble nil))
-
-;; "auctex"
 (use-package auctex
-  ;; :defer t
-  ;; :straight auctex ;; get from melpa
-  :ensure nil
+  :ensure nil ; installed via nix
   ;; :ensure (auctex :pre-build (("./autogen.sh")
   ;;				  ("./configure"
   ;;				   "--with-texmf-dir=$(dirname $(kpsexpand '$TEXMFHOME'))")
@@ -39,35 +18,21 @@
   ;;		  :build (:not elpaca--compile-info) ;; Make will take care of this step
   ;;		  :files ("*.el" "doc/*.info*" "etc" "images" "latex" "style")
   ;;		  :version (lambda (_) (require 'tex-site) AUCTeX-version))
-
-  ;; :mode ("\\.tex\\'" . LaTeX-mode)
-
-  ;; activates latex-mode in the end
-  ;; :mode ("\\.tex\\'" . tex-mode) ;; does not exist and yet works with C-c s
-  :mode ("\\.tex\\'" . TeX-latex-mode) ;; does not exist and yet works with C-c s
-  ;; :mode ("\\.tex\\'" . LaTeX-mode)
   ;; :requires reftex
-  :general (LaTeX-mode-map "C-c s" 'latex-compile)
-  ;; :general (LaTeX-mode-map "C-c s" 'latex-compile)
-  ;; :init
-  ;; setting variables related to auctex init breaks it LaTeX in elpaca
-  ;; (add-hook 'elpaca-after-init-hook (lambda () (setq latexenc-dont-use-TeX-master-flag t)))
+  :custom-face
+  ;; :set-face-foreground 'font-latex-sedate-face "#66d9ee") ; unknown keywords
+  (font-latex-sedate-face ((t (:foreground "#66d9ee")))) ; unknown keywords
+  ;; LaTeX and latex modes are the same
+  ;; :mode ("\\.tex\\'" . LaTeX-mode) ; or latex-mode
+
+  ;; somehow, putting rubbish makes TeX-command available
+  ;; (but the following becomes necessary at file bottom - except in master):
+  ;; %%% Local Variables:
+  ;; %%% mode: latex
+  ;; %%% TeX-master: "master file name without extension"
+  ;; %%% End:
+  :mode ("\\.tex\\'" . rubbish-here)
   :config
-  ;;;;;;;;;;;;;;;;
-  ;;   ELPACA   ;;
-  ;;;;;;;;;;;;;;;;
-
-  ;; (load "latex.el" nil t t)
-  ;; (load "preview-latex.el" nil t t)
-  ;; (setq-default preview-scale 1.4
-  ;;               preview-scale-function
-  ;;               (lambda () (* (/ 10.0 (preview-document-pt)) preview-scale)))
-  ;; (setq preview-auto-cache-preamble nil)
-
-  ;;;;;;;;;;;;;;;;
-  ;;   CONFIG   ;;
-  ;;;;;;;;;;;;;;;;
-
   ;; to avoid triggering prompts every time a file is opened
   (add-to-list 'safe-local-variable-values '(TeX-view-program-list . '("Evince" "evince 'output/Oeuvres à Découvrir.pdf'")))
   (add-to-list 'safe-local-variable-values '(LaTeX-command . "latex -jobname 'Oeuvres à Découvrir' -output-directory ./output"))
@@ -75,39 +40,29 @@
   ;; do not open the master with an incorrect encoding (fr characters)
   (setq latexenc-dont-use-TeX-master-flag t)
 
-  ;; (latex-preview-pane-enable) ;;requires pdf-tools
-  (setq TeX-PDF-mode t
+  ;; (latex-preview-pane-enable) ; requires pdf-tools
+  (setq TeX-save-query nil
 		;;bibtex-dialect 'biblatex ;; prefer biblatex to default
-		TeX-save-query nil
-		reftex-plug-into-AUCTeX t
-		TeX-auto-save t
-		TeX-parse-self t
-		TeX-show-compilation nil
+		;; TeX-PDF-mode t ; should be changed via a command
+		TeX-auto-save t ; save style info
+		TeX-parse-self t ; parse if no style hook found
+		;; TeX-show-compilation nil
 		;;TeX-view-program-selection '((output-pdf "PDF Tools"))
 		TeX-view-program-list '(("Evince" "evince --page-index=%(outpage) %o"))
+		reftex-plug-into-AUCTeX t
 		TeX-source-correlate-start-server t)
-  (setq-default TeX-master nil)
+  ;; (setq-default TeX-master nil)
+
+  (defun latex-compile ()
+	"Save file and compile with pdf LaTeX."
+	(interactive)
+	(save-buffer)
+	(TeX-command "LaTeX" 'TeX-master-file))
+  :general (:keymaps 'LaTeX-mode-map
+					 "C-c s" 'latex-compile)
   :hook
+  ;; ((visual-line-mode math-mode) . LaTeX-mode)
+
   ;; for latex (greek chars as unicode) \delta \Delta
   (text-mode . prettify-symbols-mode)
-
-  ;;:hook ((visual-line-mode math-mode) . LaTeX-mode)
-  (tex-mode-hook . (lambda () (setq ispell-parser 'tex)))
-  (TeX-after-compilation-finished-functions . TeX-revert-document-buffer)
-  (LaTeX-mode-hook . (lambda () (reftex-mode t) (flyspell-mode t)))
-
-  ;; (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
-  ;; (add-hook 'LaTeX-mode-hook (lambda () (reftex-mode t) (flyspell-mode t)))
-  :custom-face
-  ;;:set-face-foreground 'font-latex-sedate-face "#66d9ee") ;;unknown keywords
-  (font-latex-sedate-face ((t (:foreground "#66d9ee"))))) ;;unknown keywords
-
-(defun latex-compile ()
-  "Save file and compile with pdf LaTeX."
-  (interactive)
-  (save-buffer)
-  (TeX-command "LaTeX" 'TeX-master-file))
-
-
-;; allow remembering risky variables
-;; (defun risky-local-variable-p (sym &optional _ignored) nil)
+  (LaTeX-mode . reftex-mode))
