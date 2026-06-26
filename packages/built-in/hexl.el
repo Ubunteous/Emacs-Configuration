c0b194f5dc2f90f44f3431cedcb065cd35ca3986
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-06-26 10:15:04 +0200

    New hexl configuration

---
+++ packages/built-in/hexl.el
@@ -0,0 +1,50 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                HEXL                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package hexl
+  :ensure nil
+  :defer t
+  :general (:keymaps 'hexl-mode-map
+					 "C-t" 'hexadecimal-to-decimal))
+
+(defun decimal-to-hexadecimal ()
+  (interactive)
+  (let (xbeg xend xinput)
+	(skip-chars-backward "0-9")
+	(setq xbeg (point))
+	(skip-chars-forward "0-9")
+	(setq xend (point))
+	(setq xinput (buffer-substring-no-properties xbeg xend))
+	;; (delete-region xbeg xend)
+	;; (insert (format "%x" (string-to-number xinput)))
+
+	(message
+	 "%s
+Hexadecimal is: %x
+Decimal is: %d"
+	 this-command
+	 (string-to-number xinput)
+	 (string-to-number xinput))))
+
+(defun hexadecimal-to-decimal ()
+  (interactive)
+  (let (xbeg xend)
+	(if (region-active-p)
+		(setq xbeg (region-beginning) xend (region-end))
+	  (progn
+		(save-excursion
+		  (skip-chars-backward "[:xdigit:]#x")
+		  (setq xbeg (point))
+		  (skip-chars-forward "[:xdigit:]#x")
+		  (setq xend (point)))))
+	(when (eq xbeg xend) (user-error "%s error, input is empty." this-command))
+
+	(let ((xnum (let ((case-fold-search nil))
+				  (replace-regexp-in-string "\\`0x\\|\\`#x\\|\\`#" "" (buffer-substring-no-properties xbeg xend)))))
+	  (message
+	   "%s
+Hexadecimal is: %s
+Decimal is: %d"
+	   this-command
+	   xnum (string-to-number xnum 16)))))
