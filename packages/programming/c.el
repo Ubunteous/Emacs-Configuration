9a2a036341becf2bba0e8954cf6e7067029d81bd
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-09-02 22:44:52 +0200

    Remove C hooks and keep custom comment delimiters

--- packages/programming/c.el
+++ packages/programming/c.el
@@ -10,26 +10,29 @@
   :bind
   (:map c-ts-base-mode-map
 		("C-c C-c" . recompile))
-  :hook
-  (c++-ts-mode . (lambda ()
-		   (let ((filename (buffer-file-name)))
-			 (setq-local compile-command
-				 ;; (concat "clang -o output "
-				 (concat "g++ -o output "
-					 (if filename
-						 (shell-quote-argument
-						  filename))
-					 " && ./output"
-					 )))))
-  ((c-ts-mode c-ts-base-mode-map) . (lambda ()
-					  (let ((filename (buffer-file-name)))
+  :hook (c-ts-mode . (lambda ()
+					   (setq-local comment-end "")
+					   (setq-local comment-start "//"))))
+;; :hook
+;; (c++-ts-mode . (lambda ()
+;;				   (let ((filename (buffer-file-name)))
+;;					 (setq-local compile-command
+;;								 ;; (concat "clang -o output "
+;;								 (concat "g++ -o output "
+;;										 (if filename
+;;											 (shell-quote-argument
+;;											  filename))
+;;										 " && ./output"
+;;										 )))))
+;; ((c-ts-mode c-ts-base-mode-map) . (lambda ()
+;;									  (let ((filename (buffer-file-name)))
 
-					(setq-local compile-command
-							;; (concat "clang -o output "
-							(concat "gcc -o output "
-								(if filename
-								(shell-quote-argument
-								 filename))
-								" && ./output"
-								;; (file-name-sans-extension filename) ".out"
-								))))))
+;;										(setq-local compile-command
+;;													;; (concat "clang -o output "
+;;													(concat "gcc -o output "
+;;															(if filename
+;;																(shell-quote-argument
+;;																 filename))
+;;															" && ./output"
+;;															;; (file-name-sans-extension filename) ".out"
+;;															))))))
