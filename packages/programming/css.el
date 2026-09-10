07ce0fbe7daf7a0feb429b22a699adf08ca33bb8
pm <pierre.demetz@srm.team>
2026-09-10 16:29:46 +0200

    Add function css-browse-property-documentation

--- packages/programming/css.el
+++ packages/programming/css.el
@@ -7,6 +7,28 @@
 (use-package css-mode
   :ensure nil
   :defer t
+  :config
+  (defun css-browse-property-documentation (symbol)
+	"like css-lookup-symbol but opens documentation in a browser."
+	(interactive
+	 (list
+	  (let* ((sym (css--mdn-find-symbol))
+			 (match (seq-contains css--mdn-completion-list sym))
+			 (value (if match
+						match
+					  (completing-read (format-prompt "Describe CSS symbol" sym)
+									   css--mdn-completion-list nil nil nil
+									   'css--mdn-lookup-history sym))))
+		(if (equal value "") sym value))))
+
+	(when symbol
+	  ;; If we see a single-colon pseudo-element like ":after", turn it
+	  ;; into "::after".
+	  (when (and (eq (aref symbol 0) ?:)
+				 (member (substring symbol 1) css-pseudo-element-ids))
+		(setq symbol (concat ":" symbol)))
+	  (let ((url (format css-lookup-url-format symbol)))
+		(browse-url url))))
   :bind
   (:map css-mode-map
 		("RET" . smart-insert-semicolon)
